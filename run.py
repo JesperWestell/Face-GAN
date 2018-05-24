e4793aa3ab44b4dbcc6b1749a69d822283dd1955
JesperWestell <jesperwestell@cs231n-vm.c.concise-bloom-200217.internal>
2018-05-24 19:53:47 +0000

    Add modified (v.2) cdcgan results

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-import sys
-print(sys.path)
-
 from models.mod_cdcgan import mod_cDCGAN
 from models.cdcgan import cDCGAN
 from models.dcgan import DCGAN
@@ -10,8 +7,8 @@ from models.mod2_cdcgan import mod2_cDCGAN
 image_folder = '../data/resized_celebA/'
 attribute_folder = '../data/Anno/list_attr_celeba.txt'
 
-mod2_cdcgan = mod2_cDCGAN(image_folder, attribute_folder, cuda=False)
+mod2_cdcgan = mod2_cDCGAN(image_folder, attribute_folder, cuda=True)
 #mod_cdcgan.load(checkpoint='./mod_cdcgan_out/mod_cdcgan_epoch_24.pth')
 #mod_cdcgan.build_sample_dataset(batches=1)
-mod2_cdcgan.train(1)
+mod2_cdcgan.train(30)
 #mod_cdcgan.load_and_sample(checkpoint='./outputs/mod_cdcgan_out/mod_cdcgan_epoch_23.pth')
