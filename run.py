3374db29490f51edb9226001933fa67cdde9f3da
JesperWestell <jesper.westell@gmail.com>
2018-05-24 22:02:00 -0700

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ image_folder = '../data/resized_celebA/'
 attribute_folder = '../data/Anno/list_attr_celeba.txt'
 
 mod2_cdcgan = mod2_cDCGAN(image_folder, attribute_folder, cuda=True)
-#mod_cdcgan.load(checkpoint='./mod_cdcgan_out/mod_cdcgan_epoch_24.pth')
+#mod_cdcgan.load(checkpoint='./outputs/mod_cdcgan_out/mod_cdcgan_epoch_34.pth')
 #mod_cdcgan.build_sample_dataset(batches=1)
 mod2_cdcgan.train(30)
 #mod_cdcgan.load_and_sample(checkpoint='./outputs/mod_cdcgan_out/mod_cdcgan_epoch_23.pth')
