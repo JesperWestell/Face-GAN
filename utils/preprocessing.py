e23b47f073999cf7e6a67b23097e79805b37caf2
JesperWestell <jesper.westell@gmail.com>
2018-05-29 11:31:52 -0700

    Add ability to create subset of real images

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -3,6 +3,7 @@ import sys
 import random
 import matplotlib.pyplot as plt
 from scipy.misc import imresize
+import numpy as np
 
 def main():
     random.seed(3)
@@ -33,4 +34,31 @@ def main():
 
     print('done')
 
-main()
+def build_smaller_data_base():
+    root = os.path.dirname(sys.argv[0])
+    load = '/../../data/resized_celebA/imgs/'
+    load_root = root + load
+    save = '/../databases/real/imgs/'
+    save_root = root + save
+
+    try:
+        os.makedirs('.' + save + '/')
+    except OSError:
+        pass
+
+    if not os.path.isdir(save_root):
+        os.mkdir(save_root)
+
+    img_list = os.listdir(load_root)
+
+    size = 1400
+    indices = np.random.permutation(np.arange(len(img_list)))[:size]
+    for i,idx in enumerate(indices):
+        img = plt.imread(load_root + img_list[idx])
+        plt.imsave(fname=save_root + img_list[idx], arr=img)
+
+        if (i % 100) == 0:
+            print('%d images complete' % i)
+
+#main()
+build_smaller_data_base()
