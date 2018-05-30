455c4b8f78c1826a16e12c61b4a4b75ef5f68a87
JesperWestell <jesperwestell@cs231n-vm.c.concise-bloom-200217.internal>
2018-05-30 00:46:25 +0000

    Add results

--- compute_inception.py
+++ compute_inception.py
@@ -3,7 +3,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 
 
-root='databases/cls_gan'
+root='databases/mod_cls_gan'
 db = dset.ImageFolder(root=root,
                              transform=transforms.Compose([
                                  transforms.ToTensor(),
