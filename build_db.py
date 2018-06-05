3f99561b4e1c95cacd3525cb7421eb3f4caa0643
JesperWestell <jesperwestell@cs231n-vm.c.concise-bloom-200217.internal>
2018-06-05 17:10:43 +0000

    Add results for cdcgan and gan-cls, 40 attributes

--- build_db.py
+++ build_db.py
@@ -3,10 +3,10 @@ from models.dcgan import DCGAN
 from models.cls_gan import CLS_GAN
 from models.ac_gan import AC_GAN
 
-#gan = CLS_GAN('../data/resized_celebA/', '../data/Anno/list_attr_celeba.txt', cuda=True, subset=True)
-#gan.load(checkpoint='./outputs/cls_gan_out/cls_gan_epoch_29.pth')
-gan = AC_GAN('../data/resized_celebA/', '../data/Anno/list_attr_celeba.txt', cuda=True, subset=True)
-gan.load(checkpoint='./outputs/ac_gan_out/ac_gan_epoch_29.pth')
+gan = CLS_GAN('../data/resized_celebA/', '../data/Anno/list_attr_celeba.txt', cuda=True, subset=False)
+gan.load(checkpoint='./outputs/cls_gan_out/cls_gan_epoch_29.pth')
+#gan = cDCGAN('../data/resized_celebA/', '../data/Anno/list_attr_celeba.txt', cuda=True, subset=False)
+#gan.load(checkpoint='./outputs/cdcgan_out/cdcgan_epoch_29.pth')
 gan.build_sample_dataset(batches=200)
 
 
